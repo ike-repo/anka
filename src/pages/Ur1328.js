de4b455652ad61fc288836e18727232c5dc1a87e
ike-repo <ikeolg@gmail.com>
2022-01-16 09:21:01 -0500

    prepare for deployment

--- src/pages/Ur1328.js
+++ src/pages/Ur1328.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Aos from 'aos';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
